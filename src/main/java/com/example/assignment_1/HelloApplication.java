166a0e01186bedfa0fceb9e7a565c6f4d2dfe2dc
Sabyasachi Sadhu Turjo <ssturjo.2003@gmail.com>
2025-11-14 18:34:54 +0600

    Made the Home Screen

--- src/main/java/com/example/assignment_1/HelloApplication.java
+++ src/main/java/com/example/assignment_1/HelloApplication.java
@@ -10,10 +10,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-screen2.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("CV Builder");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
